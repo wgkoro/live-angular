b05014ed12163b98d1eb0533155d786b6ca3fe64
wgkoro <zeathwing@gmail.com>
2016-06-13 19:46:36 +0900

    controller, directive 微修正
    - ToDo完了したタイミングはng-changeでフックするよう修正 (Controller)
    - トーストの表示が正しくなるよう修正(Directive)

--- js/controllers.js
+++ js/controllers.js
@@ -8,26 +8,50 @@
         function($rootScope, todoCreator) {
         this.todoList   = [];
         this.newTodo    = '';
+        this.showEmpty  = true;
 
         var _self   = this;
         this.createNewTodo = function() {
             var todo = todoCreator.build(_self.newTodo);
             _self.todoList.push(todo);
+            _self.newTodo   = '';   // 入力欄を空に戻す
+            _self.showEmpty = false;
         };
 
         this.showEditMode   = function(todo) {
-            todo.original   = todo.todo;
+            todo.original   = todo.todo;    // 編集前に戻せるようにしておく(ng-modelにより変更内容がリアルタイムに適用されるので)
             todo.editMode   = true;
         };
 
         this.hideEditMode   = function(todo) {
             todo.editMode   = false;
-            $rootScope.$broadcast('showToast');
         };
 
         this.cancel   = function(todo) {
             todo.editMode   = false;
             todo.todo       = todo.original;
+        };
+
+        this.changeToDone   = function(todo) {
+            if (todo.done) {
+                // 完了トーストの表示を指示
+                $rootScope.$broadcast('showToast', todo.todo);
+                
+                // タスクが空かどうかのフラグ修正
+                // このへんはlodash.jsを使えば1〜2行で済む...
+                var len = _self.todoList.length;
+                var n   = 0;
+                for (var i=0;i<len;i++) {
+                    if (_self.todoList[i].done == false) {
+                        n++;
+                        break;
+                    }
+                }
+
+                if (!n) {
+                    _self.showEmpty = true;
+                }
+            }
         };
 
     }]);
--- js/directives.js
+++ js/directives.js
@@ -2,12 +2,15 @@
     'use strict';
     var app = angular.module('live.directives', []);
 
-    app.directive('toast', [function() {
+    app.directive('toast', ['$timeout', function($timeout) {
         return {
             link    : function($scope, $elem, attr) {
-                $scope.$on('showToast', function(event, data) {
+                var $p  = $elem.find('p');
+
+                $scope.$on('showToast', function(event, todo) {
+                    $p.text(todo + 'を完了しました');   // 完了したTODOにあわせて文言を変える
                     $elem.show();
-                    setTimeout(function(){
+                    $timeout(function(){
                         $elem.fadeOut();
                     }, 3000);
                 });
